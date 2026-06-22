101baa2a3ec2d8a3f3280f1bc31e00075c62b58c
Kengo Shinoda <kengo0317@g.ecc.u-tokyo.ac.jp>
2026-06-22 22:44:45 +0900

    Add ffmpeg-based video frame extraction utility

--- src/guitar_tab_agent/video/__init__.py
+++ src/guitar_tab_agent/video/__init__.py
@@ -8,9 +8,17 @@ from guitar_tab_agent.video.fretboard_calibration import (
     ManualFretboardCalibration,
     NormalizedFretboardPoint,
 )
+from guitar_tab_agent.video.frame_extractor import (
+    FrameExtractionError,
+    FrameInfo,
+    extract_frames,
+)
 
 __all__ = [
+    "FrameExtractionError",
+    "FrameInfo",
     "ImagePoint",
     "ManualFretboardCalibration",
     "NormalizedFretboardPoint",
+    "extract_frames",
 ]
